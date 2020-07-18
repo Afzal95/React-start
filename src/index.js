bd72a5b7089fefa22cece39749d29e8b8ab7ff05
Afzal95 <61465593+Afzal95@users.noreply.github.com>
2020-07-18 17:25:41 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
-//Create a react app from scratch.
-//It should display a h1 heading.
-//It should display an unordered list (bullet points).
-//It should contain 3 list elements.
-
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom";
 
-ReactDom.render(
-<div>
-  <h1>My Favourites</h1>
-  <ul>
-    <li>Cricket</li>
-    <li>Running</li>
-    <li>Coffee</li>
-  </ul>
-</div>,
-document.getElementById("root")
+const customStyle = {
+  color:"red",
+  fontSize:"20px",
+  border: "1px solid green"
+}
+
+customStyle.color = "yellow";
+
+
+ReactDOM.render(
+  <h1 style={customStyle}>Hello World!</h1>,
+  document.getElementById("root")
 );
